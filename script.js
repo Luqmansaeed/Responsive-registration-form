c5571b2fe32549658ce1d162e2848eb35e3df04b
luqman <luqmansaeed001@gmail.com>
2024-05-27 12:50:26 +0800

    registration form

--- script.js
+++ script.js
@@ -3,12 +3,14 @@ const  form = document.querySelector("form"),
         backBtn = form.querySelector(".backBtn"),
         allInput= form.querySelectorAll(".first input");
 
-nextBtn.addEventListener("click", () =>{ 
-    allInput.forEach(input =>{
-        if(input.value != ''){
-            form.classList.add()
+nextBtn.addEventListener("click", ()=> { 
+    allInput.forEach(input => {
+        if(input.value != ""){
+            form.classList.add('secActive');
+        }else{
+            form.classList.remove('secActive');
         }
     })
-
 })
+backBtn.addEventListener('click', ()=> form.classList.remove('secActive'));
         
